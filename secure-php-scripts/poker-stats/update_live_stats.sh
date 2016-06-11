fa65a4e5ad256c2f264e9cd5dcc39d65fa3fce79
finchmeister <thefinchmeister@gmail.com>
2016-06-11 12:15:33 +0000

    Good progress made on the new index
    Former-commit-id: 201ef3234515caefa6ee863bafa20e417d7f5203

--- secure-php-scripts/poker-stats/update_live_stats.sh
+++ secure-php-scripts/poker-stats/update_live_stats.sh
@@ -4,6 +4,6 @@ PIIP=finchmeister.co.uk
 
 scp pokerstatsALL.csv pi@${PIIP}:/var/www/secure-php-scripts/poker-stats/pokerstatsALL.csv
 #ssh pi@${PIIP} 'php /var/www/secure-php-scripts/poker-stats/insert_stats_into_db.php'
-#cd /var/www/secure-php-scripts/
+#cd /var/www/secure-php-scripts/poker-stats
 #echo pwd
 #php insert_stats_into_db.php
